c2e8aba6eb7aa10d962c48a74ef5a8b1ecf95f16
confortblack <confortblack@gmail.com>
2018-11-27 10:37:16 +0900

    start activity app

---
+++ clients/components/AboutUs.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AboutUs extends Component {
+  render() {
+    return (
+      <div>
+        우리를 소개합니다...
+      </div>
+    )
+  }
+}
+
+export default AboutUs;
