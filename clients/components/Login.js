3fbcccc49f9134023f7462b0e9dc4c75a2b1810b
confortblack <confortblack@gmail.com>
2018-11-27 10:31:42 +0900

    start activiting app

---
+++ clients/components/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        로그인 페이지
+      </div>
+    )
+  }
+}
+
+export default Login;
---
+++ clients/components/Meetings.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Meetings extends Component {
+  render() {
+    return (
+      <div>
+        모임 화면으로 전환...
+      </div>
+    )
+  }
+}
+
+export default Meetings;
