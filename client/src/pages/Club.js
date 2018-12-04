73f729760d62033ce4a474231a7e3fb657ade4c4
hanyeoulkim <rnltthr06@gmail.com>
2018-12-04 10:09:53 +0900

    b4 fixing redirect

--- client/src/pages/Club.js
+++ client/src/pages/Club.js
@@ -1,46 +1,45 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import ClubList from './ClubList'; 
-import axios from 'axios';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import ClubList from "./ClubList";
+import axios from "axios";
 import "../css/Club.css";
 
 class Club extends Component {
   state = {
     contents: []
-  }
-  
+  };
+
   onSearchContent = async () => {
-    const response = await axios.get('/api/content/get');
-    console.log('before render started')
+    const response = await axios.get("/api/content/get");
+    console.log("before render started");
     //console.log(response.data);
-    this.setState({ contents: response.data});
+    this.setState({ contents: response.data });
     console.log(this.state.contents);
-  }
+  };
   componentWillMount = () => {
     this.onSearchContent();
-  }
+  };
 
   render() {
-    
     return (
       <div>
-          <div>
-            <div className="outer">
-              <div className="height">
-                내 일정에 맞는 모임을 만들고 싶다면?
-              </div>
-    
-              <div className="height">
-                <Link to ="/createclub" className="btn btn-primary btn-block">모임개설 바로가기</Link>  
-              </div>
-             </div>
+        <div className="body-full">
+          <div className="outer">
+            <div className="height">내 일정에 맞는 모임을 만들고 싶다면?</div>
+
+            <div className="height">
+              <Link to="/createclub" className="btn btn-primary btn-block ui inverted purple button">
+                모임개설 바로가기
+              </Link>
+            </div>
           </div>
-          <div className="blank-height"></div>
+
+          <div className="blank-height" />
           <ClubList contents={this.state.contents} />
+        </div>
       </div>
-    )
+    );
   }
-  
 }
 
 export default Club;
